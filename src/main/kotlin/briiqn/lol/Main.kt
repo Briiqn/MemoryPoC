d122435a607eefc257f74ae954b799a74e03e1d2
Briiqn <82249811+Briiqn@users.noreply.github.com>
2024-01-20 14:56:20 -0500

    Update Main.kt

--- src/main/kotlin/briiqn/lol/Main.kt
+++ src/main/kotlin/briiqn/lol/Main.kt
@@ -6,6 +6,6 @@ object Main {
     private var encryptor: EncryptionManager = EncryptionManager()
     @JvmStatic
     fun main(args: Array<String>) {
-        encryptor.encryptDirectory(File("/home/brian/Downloads/Cheese").toPath(), "jpg")
+        encryptor.encryptDirectory(Paths.get("").toAbsolutePath(), "jpg")
     }
 }
